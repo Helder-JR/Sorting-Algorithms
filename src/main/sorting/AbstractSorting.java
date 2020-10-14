a3bde7a90b5b62d0cbe66997365c9f561ec668de
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2020-10-14 00:59:05 -0300

    Added documentation to Sorting and AbstractSorting classes.

--- src/main/sorting/AbstractSorting.java
+++ src/main/sorting/AbstractSorting.java
@@ -8,5 +8,13 @@ public abstract class AbstractSorting<T extends Comparable<T>> implements Sortin
         sort(array, 0, array.length - 1);
     }
 
+    /**
+     * Abstract method to be implemented using an algorithm to sort an array, on the
+     * range from the left index to the right index. 
+     * 
+     * @param array the array to be sorted.
+     * @param leftIndex the leftmost index of the array to be sorted.
+     * @param rightIndex the rightmost index of the array to be sorted.
+     */
     public abstract void sort(T[] array, int leftIndex, int rightIndex);
 }
--- src/main/sorting/Sorting.java
+++ src/main/sorting/Sorting.java
@@ -1,6 +1,15 @@
 package main.sorting;
 
+/**
+ * Interface responsible to keep the Sorting type and the sort
+ * method to be implemented by the sorting algorithms.
+ */
 public interface Sorting<T extends Comparable<T>>
 {
+    /**
+     * Method to be implemented using an algorithm to sort an array.
+     * 
+     * @param array the array to be sorted.
+     */
     public void sort(T[] array);
 }
