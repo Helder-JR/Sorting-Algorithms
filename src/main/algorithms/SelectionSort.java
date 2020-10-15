53a50b0a9951cdd56e97d3d18cc8c6c38008f638
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2020-10-15 19:29:23 -0300

    Added Selection Sort algorithm.

---
+++ src/main/algorithms/SelectionSort.java
@@ -0,0 +1,23 @@
+package main.algorithms;
+
+import main.sorting.AbstractSorting;
+import main.util.Util;
+
+public class SelectionSort<T extends Comparable<T>> extends AbstractSorting<T>
+{
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex)
+	{
+		for (int i = leftIndex; i <= rightIndex; i++)
+		{
+			int smaller = i;
+
+			for (int j = i + 1; j <= rightIndex; j++)
+				if (array[j].compareTo(array[smaller]) < 0)
+					smaller = j;
+			
+			Util.swap(array, i, smaller);
+		}
+	}
+}
