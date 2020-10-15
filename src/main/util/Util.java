3044396c3ef428f3748f904a4b28f72ab2ef3151
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2020-10-15 20:00:59 -0300

    Changed Util and UtilTest indentation from spaces to tabs.

--- src/main/util/Util.java
+++ src/main/util/Util.java
@@ -5,70 +5,70 @@ package main.util;
  */
 public abstract class Util
 {
-    /**
-     * Swaps two elements in an array.
-     * 
-     * @param <T> the array type.
-     * @param array the array that contains the elements to be swapped.
-     * @param first the element that will be swapped.
-     * @param second the other element that will be swapped.
-     */
-    public static <T> void swap(T[] array, int first, int second)
-    {
-        T aux = array[first];
-        array[first] = array[second];
-        array[second] = aux;
-    }
+	/**
+	 * Swaps two elements in an array.
+	 * 
+	 * @param <T> the array type.
+	 * @param array the array that contains the elements to be swapped.
+	 * @param first the element that will be swapped.
+	 * @param second the other element that will be swapped.
+	 */
+	public static <T> void swap(T[] array, int first, int second)
+	{
+		T aux = array[first];
+		array[first] = array[second];
+		array[second] = aux;
+	}
 
-    /**
-     * Finds the index of the biggest element in an array.
-     * 
-     * @param <T> the array type.
-     * @param array the array in which the biggest element will be found.
-     * @return the index of the biggest element.
-     */
-    public static <T extends Comparable<T>> int maximum(T[] array)
-    {
-        int maximum = 0;
+	/**
+	 * Finds the index of the biggest element in an array.
+	 * 
+	 * @param <T> the array type.
+	 * @param array the array in which the biggest element will be found.
+	 * @return the index of the biggest element.
+	 */
+	public static <T extends Comparable<T>> int maximum(T[] array)
+	{
+		int maximum = 0;
 
-        for (int i = 1; i < array.length; i++)
-            if (array[i].compareTo(array[maximum]) > 0)
-                maximum = i;
-        
-        return maximum;
-    }
+		for (int i = 1; i < array.length; i++)
+			if (array[i].compareTo(array[maximum]) > 0)
+				maximum = i;
+		
+		return maximum;
+	}
 
-    /**
-     * Finds the index of the smallest element in an array.
-     * 
-     * @param <T> the array type.
-     * @param array the array in which the smallest element will be found.
-     * @return the index of the smallest element.
-     */
-    public static <T extends Comparable<T>> int minimum(T[] array)
-    {
-        int minimum = 0;
-        
-        for (int i = 1; i < array.length; i++)
-            if (array[i].compareTo(array[minimum]) < 0)
-            minimum = i;
-        
-            return minimum;
-        }
-        
-    /**
-     * Verifies if an array is valid to be sorted. To be considered valid, an array must have a size
-     * bigger than one (because a unique element array is already sorted), not null, with its left
-     * index being at least zero and smaller than the right index.
-     * 
-     * @param <T> the array type;
-     * @param array the array to be verified
-     * @param leftIndex the leftmost index of the array.
-     * @param rightIndex the rightmost index of the array.
-     * @return a boolean {@code true} in case of the array being valid or {@code false} otherwise.
-     */
-    public static <T extends Comparable<T>> boolean isValid(T[] array, int leftIndex, int rightIndex)
-    {
-        return (array != null && array.length > 1 && leftIndex >= 0 && leftIndex < rightIndex);  
-    }
+	/**
+	 * Finds the index of the smallest element in an array.
+	 * 
+	 * @param <T> the array type.
+	 * @param array the array in which the smallest element will be found.
+	 * @return the index of the smallest element.
+	 */
+	public static <T extends Comparable<T>> int minimum(T[] array)
+	{
+		int minimum = 0;
+		
+		for (int i = 1; i < array.length; i++)
+			if (array[i].compareTo(array[minimum]) < 0)
+			minimum = i;
+		
+			return minimum;
+		}
+		
+	/**
+	 * Verifies if an array is valid to be sorted. To be considered valid, an array must have a size
+	 * bigger than one (because a unique element array is already sorted), not null, with its left
+	 * index being at least zero and smaller than the right index.
+	 * 
+	 * @param <T> the array type;
+	 * @param array the array to be verified
+	 * @param leftIndex the leftmost index of the array.
+	 * @param rightIndex the rightmost index of the array.
+	 * @return a boolean {@code true} in case of the array being valid or {@code false} otherwise.
+	 */
+	public static <T extends Comparable<T>> boolean isValid(T[] array, int leftIndex, int rightIndex)
+	{
+		return (array != null && array.length > 1 && leftIndex >= 0 && leftIndex < rightIndex);  
+	}
 }
--- src/test/util/UtilTest.java
+++ src/test/util/UtilTest.java
@@ -10,29 +10,29 @@ import main.util.Util;
 
 public class UtilTest
 {
-    Integer[] array;
+	Integer[] array;
 
-    @Before
-    public void setUp()
-    {
-        this.array = new Integer[] { 2, 4, 6, 8, 0, 9, 7, 5, 3, 1 };
-    }
+	@Before
+	public void setUp()
+	{
+		this.array = new Integer[] { 2, 4, 6, 8, 0, 9, 7, 5, 3, 1 };
+	}
 
-    @Test
-    public void testMaximum()
-    {
-        Assert.assertEquals(5, Util.maximum(array));
-    }
+	@Test
+	public void testMaximum()
+	{
+		Assert.assertEquals(5, Util.maximum(array));
+	}
 
-    @Test
-    public void testMinimum()
-    {
-        Assert.assertEquals(4, Util.minimum(array));
-    }
+	@Test
+	public void testMinimum()
+	{
+		Assert.assertEquals(4, Util.minimum(array));
+	}
 
-    @Test
-    public void testIsValidArray()
-    {
-        assertTrue(Util.isValid(array, 0, array.length - 1));
-    }
+	@Test
+	public void testIsValidArray()
+	{
+		assertTrue(Util.isValid(array, 0, array.length - 1));
+	}
 }
