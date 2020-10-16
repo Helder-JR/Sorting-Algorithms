7883face19bcdd468c342463cdbab74846cf6c12
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2020-10-16 13:27:50 -0300

    Added more tests to SortingAlgorithmTest.

--- src/test/algorithms/SortingAlgorithmTest.java
+++ src/test/algorithms/SortingAlgorithmTest.java
@@ -11,12 +11,13 @@ import main.algorithms.*;
 
 public class SortingAlgorithmTest
 {
-	private Integer[] reversedArray;
-	private Integer[] oddSizeArray;
-	private Integer[] evenSizeArray;
 	private Integer[] emptyArray;
 	private Integer[] equalValuesArray;
+	private Integer[] evenSizeArray;
+	private Integer[] negativeArray;
+	private Integer[] oddSizeArray;
 	private Integer[] repeatedValuesArray;
+	private Integer[] reversedArray;
 	private Integer[] uniqueElementArray;
 
 	Sorting<Integer> implementation;
@@ -27,12 +28,13 @@ public class SortingAlgorithmTest
 	@Before
 	public void setUp()
 	{
-		this.reversedArray = new Integer[] { 97, 72, 66, 50, 43, 31, 25, 11, 8 };
-		this.oddSizeArray = new Integer[] { 6, 41, 32, 7, 26, 4, 37, 49, 11, 18, 36 };
-		this.evenSizeArray = new Integer[] { 30, 28, 7, 29, 11, 26, 4, 22, 23, 31 };
 		this.emptyArray = new Integer[] {};
 		this.equalValuesArray = new Integer[] { 6, 6, 6, 6, 6, 6 };
+		this.evenSizeArray = new Integer[] { 30, 28, 7, 29, 11, 26, 4, 22, 23, 31 };
+		this.negativeArray = new Integer[] { -65, -12, 28, -7, 96, 55, -1, 43, 10, 0 };
+		this.oddSizeArray = new Integer[] { 6, 41, 32, 7, 26, 4, 37, 49, 11, 18, 36 };
 		this.repeatedValuesArray = new Integer[] { 4, 9, 3, 4, 0, 5, 1, 4 };
+		this.reversedArray = new Integer[] { 97, 72, 66, 50, 43, 31, 25, 11, 8 };
 		this.uniqueElementArray = new Integer[] {7};
 
 		this.getImplementation();
@@ -43,7 +45,7 @@ public class SortingAlgorithmTest
 	 */
 	private void getImplementation()
 	{
-		this.implementation = new MergeSort<>();
+		this.implementation = null;
 	}
 
 	/**
@@ -104,5 +106,11 @@ public class SortingAlgorithmTest
 	public void teste07()
 	{
 		genericTest(this.uniqueElementArray);
+	}
+
+	@Test
+	public void teste08()
+	{
+		genericTest(this.negativeArray);
 	}
 }
