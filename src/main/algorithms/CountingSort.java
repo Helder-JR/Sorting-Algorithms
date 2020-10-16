cbbcd51f82210f3f94136ce85730fc6351484742
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2020-10-16 13:29:20 -0300

    Changed Counting Sort to work with negative integers.

--- src/main/algorithms/CountingSort.java
+++ src/main/algorithms/CountingSort.java
@@ -1,7 +1,7 @@
 package main.algorithms;
 
 import main.sorting.AbstractSorting;
-import main.util.Util;
+import static main.util.Util.*;
 
 /**
  * Class responsible for implementing the sort method from {@link AbstractSorting}
@@ -15,16 +15,18 @@ public class CountingSort extends AbstractSorting<Integer>
     @Override
     public void sort(Integer[] array, int leftIndex, int rightIndex)
     {
-        if (Util.isValid(array, leftIndex, rightIndex))
+        if (isValid(array, leftIndex, rightIndex))
         {
-            int max = Util.maximum(array);
-            Integer[] counting = new Integer[array[max] + 1];
+            int max = array[maximum(array)];
+            int min = Math.abs(array[minimum(array)]);
+
+            Integer[] counting = new Integer[min + 1 + max];
     
             for (int i = leftIndex; i < counting.length; i++)
                 counting[i] = 0;
     
             for (int i = leftIndex; i <= rightIndex; i++)
-                counting[array[i]]++;
+                counting[array[i] + min]++;
             
             for (int i = leftIndex + 1; i < counting.length; i++)
                 counting[i] += counting[i - 1];
@@ -33,8 +35,8 @@ public class CountingSort extends AbstractSorting<Integer>
     
             for (int i = rightIndex; i >= leftIndex; i--)
             {
-                result[counting[array[i]] - 1] = array[i];
-                counting[array[i]]--;
+                result[counting[array[i] + min] - 1] = array[i];
+                counting[array[i] + min]--;
             }
     
             for (int i = leftIndex; i <= rightIndex; i++)
