71816069988785b576b88b8fac19f2694e22d848
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2020-10-18 17:24:18 -0300

    Minimal changes in Insertion Sort.

--- src/main/algorithms/InsertionSort.java
+++ src/main/algorithms/InsertionSort.java
@@ -5,7 +5,6 @@ import main.util.Util;
 
 public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T>
 {
-
     @Override
     public void sort(T[] array, int leftIndex, int rightIndex)
     {
